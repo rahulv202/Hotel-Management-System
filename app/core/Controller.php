497dfed89b6f565894ea24fabbe91dce149a6ef3
Rahul Vaidya <rahulvaidya202@gmail.com>
2024-12-16 16:47:35 +0530

    Add views header and footer

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -7,9 +7,9 @@ class Controller
     public function views($view, $data = [])
     {
         extract($data);
-        require_once APP_PATH . "/app/views/header.php";
+        require_once APP_PATH . "/app/views/layouts/header.php";
         require_once APP_PATH . 'views/' . $view . '.php';
-        require_once APP_PATH . "/app/views/footer.php";
+        require_once APP_PATH . "/app/views/layouts/footer.php";
     }
 
     protected function redirect($url)
